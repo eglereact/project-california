fdc0c7f364a387b6252200517c48bbf295a360d4
eglereact <eglereactjs@gmail.com>
2024-03-12 12:14:29 +0200

    fix menu lang btn

--- app.js
+++ app.js
@@ -6,3 +6,12 @@ const langbox = document.querySelector(".btn-box");
 footerLangBtn.addEventListener("click", () => {
   langbox.classList.toggle("show-lang-btn");
 });
+
+// menu language button functionality
+
+const menuLangBtn = document.querySelector(".menu-lang-btn");
+const menuBtnBox = document.querySelector(".menu-btn-box");
+
+menuLangBtn.addEventListener("click", () => {
+  menuBtnBox.classList.toggle("menu-btn-show");
+});
